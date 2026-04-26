895e5bf54e4414eaf5cfa39b8acb212bb9df26d8
Fahad Ahmed <116654916+FahadAhmed05@users.noreply.github.com>
2026-04-26 18:42:35 +0500

    integrate images

--- src/components/PlateArt.jsx
+++ src/components/PlateArt.jsx
@@ -1,33 +1,40 @@
+import sampleImage1 from "../assets/images/sampleImage1.jpg";
+import sampleImage2 from "../assets/images/sampleImage2.jpg";
+import sampleImage3 from "../assets/images/sampleImage3.jpg";
+
 function PlateArt({ variant }) {
   if (variant === 'rice-bowl') {
     return (
-      <div className="plate-art rice-bowl">
-        <div className="plate"></div>
-        <div className="food rice"></div>
-        <div className="food stew"></div>
-      </div>
+      <img src={sampleImage2} alt={variant} />
+      // <div className="plate-art rice-bowl">
+      //   <div className="plate"></div>
+      //   <div className="food rice"></div>
+      //   <div className="food stew"></div>
+      // </div>
     )
   }
 
   if (variant === 'curry-platter') {
     return (
-      <div className="plate-art curry-platter">
-        <div className="plate"></div>
-        <div className="food naan"></div>
-        <div className="food curry"></div>
-        <div className="food garnish"></div>
-      </div>
+      <img src={sampleImage1} alt={variant} />
+      // <div className="plate-art curry-platter">
+      //   <div className="plate"></div>
+      //   <div className="food naan"></div>
+      //   <div className="food curry"></div>
+      //   <div className="food garnish"></div>
+      // </div>
     )
   }
 
   return (
-    <div className="plate-art mix-grill">
-      <div className="board"></div>
-      <div className="skewer skewer-1"></div>
-      <div className="skewer skewer-2"></div>
-      <div className="bowl bowl-1"></div>
-      <div className="bowl bowl-2"></div>
-    </div>
+    <img src={sampleImage3} alt={variant} />
+    // <div className="plate-art mix-grill">
+    //   <div className="board"></div>
+    //   <div className="skewer skewer-1"></div>
+    //   <div className="skewer skewer-2"></div>
+    //   <div className="bowl bowl-1"></div>
+    //   <div className="bowl bowl-2"></div>
+    // </div>
   )
 }
 
